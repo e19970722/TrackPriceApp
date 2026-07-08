53b86deff7a1bc53eaa56c80171201a3a6f0f277
Yen Lin <e19970722@gmail.com>
2026-07-08 11:06:37 +0800

    [iOS] add package: KingFisher

--- iOS/Features/Package.swift
+++ iOS/Features/Package.swift
@@ -16,6 +16,10 @@ let package = Package(
             url: "https://github.com/google/GoogleSignIn-iOS",
             from: "8.0.0"
         ),
+        .package(
+            url: "https://github.com/onevcat/Kingfisher.git",
+            from: "8.10.0"
+        ),
     ],
     targets: [
         .target(
@@ -23,6 +27,7 @@ let package = Package(
             dependencies: [
                 .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
                 .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
+                .product(name: "Kingfisher", package: "Kingfisher"),
             ],
             resources: [
                 .process("Resources/RipeColors.xcassets"),
